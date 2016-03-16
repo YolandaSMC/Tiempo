0a504a2ab71e69d5104fcce8f2561a8f86df3ae4
YolandaSMC <Yolanda@Yolanda-PC>
2016-03-16 13:10:40 +0100

    Añadido los dos fragment que serán las dos pantallas de la app

---
+++ app/src/main/java/com/example/yolanda/tiempo/BusquedaFragment.java
@@ -0,0 +1,67 @@
+package com.example.yolanda.tiempo;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class BusquedaFragment extends Fragment {
+
+
+    private OnFragmentInteractionListener mListener;
+
+    public BusquedaFragment() {
+
+    }
+
+    public static BusquedaFragment newInstance(String param1, String param2) {
+        BusquedaFragment fragment = new BusquedaFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_busqueda,container,false);
+        return view;
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
